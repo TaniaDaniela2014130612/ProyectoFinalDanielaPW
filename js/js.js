c12d9d84e309c77d8012677bddd6ebc193d1c08f
TaniaDaniela2014130612 <86133134+TaniaDaniela2014130612@users.noreply.github.com>
2021-06-19 01:42:08 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,26 +1,35 @@
-      const formulario= document.getElementById("forma");
-      const db = firebase.firestore();
-      const subirDatos = (nombre, apellido)=>db.collection("personas").doc().set({nombre, apellido})
-      const verDatos = ()=>db.collection("personas").get()
-      formulario.addEventListener('submit', async(e)=>{
-          e.preventDefault ()
-            const nombre=formulario["nombre"], apellido=formulario["apellido"];
-            await subirDatos(nombre.value, apellido.value)
-            formulario.reset()
-            imprimir()
-      })
-      
-      imprimir= async ()=>{
-        datos.innerHTML=''
-        const query = await verDatos()
-        query.forEach(element => {
-            datos.innerHTML+=`<p>${element.data().nombre} </p>
-            <p>${element.data().apellido} </p>`
-            
-        });
-      }
+datos = document.getElementById("datos");
+const formulario = document.getElementById("forma");
+const db = firebase.firestore();
+const subirDatos = (nombre, apellido) =>
+db.collection("personas").doc().set({ nombre, apellido });
+const verDatos = () => db.collection("personas").get();
 
-      datos=document.getElementById("datos");
-      window.addEventListener("DOMContentLoaded", async()=>{
-          imprimir()
-      })
+imprimir = async () => {
+    datos.innerHTML = "";
+  const query = await verDatos();
+  query.forEach((element) => {
+      datos.innerHTML += `<div class="card col-3 m-4">
+      <div class="card-body">
+            <p><label class="col-form-label"> Nombre:</label> ${
+                element.data().nombre
+            } </p>
+            <p><label class="col-form-label"> Nombre:</label> ${
+                element.data().apellido
+            } </p>
+            </div>
+            </div>`;
+        });
+    };
+    formulario.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const nombre = formulario["nombre"],
+        apellido = formulario["apellido"];
+      await subirDatos(nombre.value, apellido.value);
+      formulario.reset();
+      imprimir();
+    });
+
+    window.addEventListener("DOMContentLoaded", async () => {
+        imprimir();
+    });
